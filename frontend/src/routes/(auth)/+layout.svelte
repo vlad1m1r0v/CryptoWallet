4a027b260c54b874c30f4ae2375328d6f51ae5a3
ant100pshnik <mypochtav@gmail.com>
2025-09-22 15:07:48 +0300

    add login and register svelte pages

---
+++ frontend/src/routes/(auth)/+layout.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    let {children} = $props();
+</script>
+
+<svelte:head>
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="vuexy/css/plugins/forms/form-validation.css">
+    <link rel="stylesheet" type="text/css" href="vuexy/css/pages/page-auth.css">
+    <!-- END: Page CSS-->
+    <title>Authentication</title>
+</svelte:head>
+
+<div class="content-overlay"></div>
+<div class="content-wrapper">
+    <div class="content-header row">
+    </div>
+    <div class="content-body">
+        <div class="auth-wrapper auth-v1 px-2">
+            <div class="auth-inner py-2">
+                <!-- Login v1 -->
+                <div class="card mb-0">
+                    <div class="card-body">
+                        {@render children?.()}
+                    </div>
+                </div>
+                <!-- /Login v1 -->
+            </div>
+        </div>
+
+    </div>
+</div>
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    let {children} = $props();
+
+</script>
+
+<svelte:head>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
+    <meta name="description"
+          content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords"
+          content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="PIXINVENT">
+    <link rel="apple-touch-icon" href="vuexy/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="vuexy/images/ico/favicon.ico">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600"
+          rel="stylesheet">
+
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="vuexy/vendors/css/vendors.min.css">
+    <!-- END: Vendor CSS-->
+
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="vuexy/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="vuexy/css/bootstrap-extended.css">
+    <link rel="stylesheet" type="text/css" href="vuexy/css/colors.css">
+    <link rel="stylesheet" type="text/css" href="vuexy/css/components.css">
+    <link rel="stylesheet" type="text/css" href="vuexy/css/themes/dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="vuexy/css/themes/bordered-layout.css">
+    <link rel="stylesheet" type="text/css" href="vuexy/css/themes/semi-dark-layout.css">
+
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="vuexy/css/core/menu/menu-types/vertical-menu.css">
+    <link rel="stylesheet" type="text/css" href="vuexy/css/plugins/forms/form-validation.css">
+    <link rel="stylesheet" type="text/css" href="vuexy/css/pages/page-auth.css">
+    <!-- END: Page CSS-->
+</svelte:head>
+{@render children?.()}
+
+
