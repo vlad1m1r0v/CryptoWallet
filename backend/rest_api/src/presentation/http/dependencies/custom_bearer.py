86cfe49768d9316d9e769d20bf66e64ddc6d5249
ant100pshnik <mypochtav@gmail.com>
2025-10-24 06:28:38 +0300

    improved custom_bearer for swagger

--- backend/rest_api/src/presentation/http/dependencies/custom_bearer.py
+++ backend/rest_api/src/presentation/http/dependencies/custom_bearer.py
@@ -1,14 +1,11 @@
-from fastapi import Request
-from fastapi.security.utils import get_authorization_scheme_param
+from fastapi import Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from src.infrastructure.exceptions.auth import AccessTokenNotProvidedException
 
+bearer_scheme = HTTPBearer(auto_error=False)
 
-async def custom_bearer(request: Request) -> str:
-    authorization: str = request.headers.get("Authorization")
-    scheme, credentials = get_authorization_scheme_param(authorization)
-
-    if not authorization or scheme.lower() != "bearer" or not credentials:
+async def custom_bearer(credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme)) -> str:
+    if not credentials or credentials.scheme.lower() != "bearer":
         raise AccessTokenNotProvidedException()
-
-    return credentials
+    return credentials.credentials
