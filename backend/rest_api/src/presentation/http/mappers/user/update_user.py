8461a3921c4a8d6379990be26132a7793339bf07
ant100pshnik <mypochtav@gmail.com>
2025-11-04 21:06:10 +0200

    refactoring

--- backend/rest_api/src/presentation/http/mappers/user/update_user.py
+++ backend/rest_api/src/presentation/http/mappers/user/update_user.py
@@ -31,5 +31,5 @@ class UpdateUserMapper(
         return UpdateUserResponseSchema(
             username=dto["username"],
             email=dto["email"],
-            avatar_url=f"{config.s3.base_file_url}/{dto['avatar_filename']}"
+            avatar_url=f"{config.s3.base_file_url}/{dto['avatar_filename']}" if dto["avatar_filename"] else None,
         )
