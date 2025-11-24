17927cfd709751d4de76e85701f13ad68c716f1c
ant100pshnik <mypochtav@gmail.com>
2025-11-24 16:07:41 +0200

    remove unique constraint for username

--- backend/rest_api/src/infrastructure/persistence/database/models/user.py
+++ backend/rest_api/src/infrastructure/persistence/database/models/user.py
@@ -15,7 +15,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(UUID(as_uuid=True), primary_key=True)
-    username = Column(String(32), nullable=False, unique=True)
+    username = Column(String(32), nullable=False, unique=False)
     email = Column(String(50), nullable=False, unique=True, index=True)
     password_hash = Column(LargeBinary, nullable=False)
     avatar_filename = Column(String(50), nullable=True)
